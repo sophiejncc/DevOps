e1a0082d526b2aebc2d76477871aa47ccf7e7bc3
Sophie Joyce <sophie.joyce@nottinghamcity.gov.uk>
2026-06-08 19:31:50 +0100

    Red - failing tests committed

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+# Arrange
+# Act
+# Assert
+
+def test_high_priority_idea():
+    idea = Idea("High priority", impact = 5, resource = 1, alignment = 5)
+    assert idea.score() >= 4
+
+def test_low_priority_idea():
+    idea = Idea("Low priority", impact = 1, resource = 5, alignment = 1)
+    assert idea.score() < 2
+
+def test_status_update():
+    engine = DecisionEngine()
+    idea = Idea("Test idea", impact = 5, resource = 1, alignment = 5)
+
+    engine.add_idea(idea)
+    engine.update_status()
+
+    assert idea.status =="Approved"
